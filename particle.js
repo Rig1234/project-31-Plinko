8c4702524be3674c3409234f4c599e6bc32b8d18
Rig1234 <78023252+Rig1234@users.noreply.github.com>
2021-05-20 12:44:19 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,24 @@
+class Particle {
+    constructor(x,y,r){
+        var options = {
+            "friction":0.01,
+            "restitution":0.02,
+            "density":0.001
+        }
+
+      this.radius = r
+      this.body = Bodies.circle(x,y,r,options);
+     this.color = color(random(0,255),random(0,255),random(0,255));
+      World.add(world,this.body);
+
+    }
+    display(){
+        push();
+        translate(this.body.position.x,this.body.position.y);
+        rotate(this.body.angle);
+        //fill("black");
+        //stroke("red");
+        ellipse(0,0,this.body.r,this.body.r);
+        pop()
+    }
+}
